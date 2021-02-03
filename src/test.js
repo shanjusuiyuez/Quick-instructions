3174da6db8d3a192168bb8ecea79d4629c8c3bd1
3489840593@qq.com <3489840593@qq.com>
2021-02-03 21:13:16 +0800

    del

--- src/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var win_compilations_1 = require("./win_compilations");
-var lobool = "yes";
-var sss = "www.baidu.com";
-win_compilations_1.ping(sss, lobool);
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-import { ping } from "./win_compilations";
-var lobool = "yes";
-var sss = "www.baidu.com";
-ping(sss,lobool);
