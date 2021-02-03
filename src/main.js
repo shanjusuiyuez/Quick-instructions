8565a50723e89d551ce35f2a1924521ade8fc2ff
3489840593@qq.com <3489840593@qq.com>
2021-02-03 21:18:22 +0800

    del

--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//自动更新模块
-require('update-electron-app')({
-    repo: 'https://github.com/shanjusuiyuez/Quick-instructions/repo',
-    updateInterval: '1 day',
-    logger: require('electron-log.txt')
-});
-console.clear();
